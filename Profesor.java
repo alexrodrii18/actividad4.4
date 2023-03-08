954d2c94ec5ab912d7d63da39e86d4bed94ecbbc
alexrodrii18 <116663505+alexrodrii18@users.noreply.github.com>
2023-03-08 18:39:33 +0100

    Update Profesor.java

--- Profesor.java
+++ Profesor.java
@@ -1,54 +1,26 @@
+import java.util.ArrayList;
 import java.util.List;
-/**
- * Esta clase representa a un profesor que hereda de la clase Persona.
- */
-public class Profesor extends Persona{
-    int edad;
-    String nombre;
-    List<Prestamo> prestamos;
+
+public class Profesor extends Persona {
+    protected List<String> prestamos = new ArrayList<>();
     /**
-     * Constructor que inicializa el número de telefono y el nombre del profesor
-     * @param telefono El número de telefono del profesor.
-     * @param nombre El nombre del profesor.
+     * Constructor de la clase Profesor que inicializa diferentes atributos
+     * @param nombre Nombre del profesor
+     * @param edad Edad del profesor
+     * @param telefono Telefono del profesor
      */
-    public Profesor(String nombre,int edad, String telefono) {
-        super(telefono);
-        this.nombre=nombre;
-        this.edad = edad;
+    public Profesor(String nombre, int edad, String telefono ) {
+        super(nombre, edad, telefono);
     }
+
     /**
-     * Obtiene la edad del profesor
-     * @return La edad del profesor.
+     * Metoto sobreescrito toString para dar formato al objeto Profesor
+     * @return El formato en una cadena
      */
-    // Agregar los métodos get y set para el atributo "edad"
-    public int getEdad() {
-        return edad;
-    }
-    /**
-     * Establece la edad del profesor.
-     * @param edad La nueva edad del profesor.
-     */
-    public void setEdad(int edad) {
-        this.edad = edad;
-    }
-    /**
-     * Imprime la informacion personal del profesor.
-     */
-    public void imprimirInformacionPersonal(){
-        System.out.println("Nombre: " + nombre);
-        System.out.println("Edad: " + edad);
-        System.out.println("Teléfono: " + telefono);
-    }
-    /**
-     * Imprime toda la información del profesor, incluyendo sus prestamos.
-     */
-    public void imprimirTodaLaInformacion(){
-        System.out.println("Nombre: " + nombre);
-        System.out.println("Edad: " + edad);
-        System.out.println("Teléfono: " + telefono);
-        for (Prestamo p: prestamos) {
-            System.out.println(p);
-        }
+    @Override
+    public String toString() {
+        return String.format("Nombre: %s, edad %d, y su telefono %s" , nombre, edad, telefono);
+
     }
 }
 
