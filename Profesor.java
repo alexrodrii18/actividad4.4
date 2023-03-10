0a1cf3bc3156c9e9c58dd355f2f11b397f3144ac
alexrodrii18 <116663505+alexrodrii18@users.noreply.github.com>
2023-03-10 11:41:58 +0100

    Update Profesor.java

--- Profesor.java
+++ Profesor.java
@@ -1,28 +1,76 @@
-import java.util.ArrayList;
 import java.util.List;
+
 /**
- * Clase que representa a un Profesor y extiende de la clase Persona
+ * Clase que representa a un profesor y extiende de la clase Persona
  */
 public class Profesor extends Persona {
-    protected List<String> prestamos = new ArrayList<>();
+    private String nombre;
+    private int edad;
+    private List<Prestamo> prestamos;
+
     /**
-     * Constructor de la clase Profesor que inicializa diferentes atributos
+     * Constructor de la clase Profesor que inicializa los atributos numeroDeTelefono, nombre y edad
+     * @param numeroDeTelefono numeroDeTelefono del profesor
      * @param nombre Nombre del profesor
      * @param edad Edad del profesor
-     * @param telefono Telefono del profesor
      */
-    public Profesor(String nombre, int edad, String telefono ) {
-        super(nombre, edad, telefono);
+    public Profesor(String numeroDeTelefono, String nombre, int edad) {
+        super(numeroDeTelefono);
+        this.nombre = nombre;
+        this.edad = edad;
     }
 
     /**
-     * Metoto sobreescrito toString para dar formato al objeto Profesor
-     * @return El formato en una cadena
+     * Getter de nombre
+     * @return Nombre del profesor
      */
-    @Override
-    public String toString() {
-        return String.format("Nombre: %s, edad %d, y su telefono %s" , nombre, edad, telefono);
+    public String getNombre() {
+        return nombre;
+    }
 
+    /**
+     * Setter de nombre
+     * @param nombre Nombre del profesor
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * Getter de edad
+     * @return Edad del profesor
+     */
+    public int getEdad() {
+        return edad;
+    }
+
+    /**
+     * Setter de edad
+     * @param edad Edad del profesor
+     */
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    /**
+     * Metodo que printea la informacion personal acerca del profesor
+     */
+    public void printInformacionPersonal() {
+        System.out.println("Nombre: " + getNombre());
+        System.out.println("Edad: " + getEdad());
+        System.out.println("Teléfono: " + getNumeroDeTelefono());
+    }
+
+    /**
+     * * Metodo que printea toda la informacion acerca del profesor
+     */
+    public void printTodaLaInformacion() {
+        System.out.println("Nombre: " + getNombre());
+        System.out.println("Edad: " + getEdad());
+        System.out.println("Teléfono: " + getNumeroDeTelefono());
+        for (Prestamo p : prestamos) {
+            System.out.println(p);
+        }
     }
 }
 
